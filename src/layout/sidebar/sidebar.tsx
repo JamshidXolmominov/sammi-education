867a87bb8fa85d2013fbdf2d5a30969105a8d85d
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-03-14 12:21:50 +0300

    High OrderComponents & Layout

---
+++ src/layout/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+import { SidebarProps } from './sidebar.props';
+
+const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+	return <div {...props}>Sidebar</div>;
+};
+
+export default Sidebar;
