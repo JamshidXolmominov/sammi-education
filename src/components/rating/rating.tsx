0ba6115020fd8e2f9dcc8fec780625975ef59506
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-03-19 14:38:04 +0300

    Validation React Hook Form

--- src/components/rating/rating.tsx
+++ src/components/rating/rating.tsx
@@ -1,56 +1,65 @@
 import styles from './rating.module.css';
 import cn from 'classnames';
 import { RatingProps } from './rating.props';
-import { useEffect, useState } from 'react';
+import { ForwardedRef, forwardRef, useEffect, useState } from 'react';
 import StarIcon from './star.svg';
 
-const Rating = ({ rating, isEditabled = false, setRating, ...props }: RatingProps): JSX.Element => {
-	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+const Rating = forwardRef(
+	({ rating, isEditabled = false, setRating, error, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+		const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
-	useEffect(() => {
-		renderRating(rating);
-	}, [rating]);
+		useEffect(() => {
+			renderRating(rating);
+		}, [rating]);
 
-	const renderRating = (currentRating: number) => {
-		const updateArray = ratingArray.map((r: JSX.Element, idx: number) => (
-			<span
-				className={cn(styles.star, {
-					[styles.filled]: idx < currentRating,
-					[styles.editable]: isEditabled,
+		const renderRating = (currentRating: number) => {
+			const updateArray = ratingArray.map((r: JSX.Element, idx: number) => (
+				<span
+					className={cn(styles.star, {
+						[styles.filled]: idx < currentRating,
+						[styles.editable]: isEditabled,
+					})}
+					onMouseEnter={() => changeRatingDisplay(idx + 1)}
+					onMouseLeave={() => changeRatingDisplay(rating)}
+					onClick={() => clickRatingHandler(idx + 1)}
+				>
+					<StarIcon />
+				</span>
+			));
+
+			setRatingArray(updateArray);
+		};
+
+		const changeRatingDisplay = (index: number) => {
+			if (!isEditabled) {
+				return;
+			}
+
+			renderRating(index);
+		};
+
+		const clickRatingHandler = (index: number) => {
+			if (!isEditabled || !setRating) {
+				return;
+			}
+			setRating(index);
+		};
+
+		return (
+			<div
+				className={cn(styles.rating, {
+					[styles.error]: error,
 				})}
-				onMouseEnter={() => changeRatingDisplay(idx + 1)}
-				onMouseLeave={() => changeRatingDisplay(rating)}
-				onClick={() => clickRatingHandler(idx + 1)}
+				ref={ref}
+				{...props}
 			>
-				<StarIcon />
-			</span>
-		));
-
-		setRatingArray(updateArray);
-	};
-
-	const changeRatingDisplay = (index: number) => {
-		if (!isEditabled) {
-			return;
-		}
-
-		renderRating(index);
-	};
-
-	const clickRatingHandler = (index: number) => {
-		if (!isEditabled || !setRating) {
-			return;
-		}
-		setRating(index);
-	};
-
-	return (
-		<div className={styles.rating} {...props}>
-			{ratingArray.map((rating, idx) => (
-				<span key={idx}>{rating}</span>
-			))}
-		</div>
-	);
-};
+				{ratingArray.map((rating, idx) => (
+					<span key={idx}>{rating}</span>
+				))}
+				{error && <span className={styles.errorMessage}>{error.message}</span>}
+			</div>
+		);
+	}
+);
 
 export default Rating;
